30659a629cbafb0f45f9b2ec0829765f0d9d8dd3
Chris Mason <littlecmason@yahoo.com>
2016-08-26 20:07:42 -0400

    save

--- dungeon.py
+++ dungeon.py
@@ -10,106 +10,114 @@ def clear():
 def build_dungeon():
   global dungeon
   
-  dungeon = [[c.red + '#' for x in range(10)] for y in range(10)]
+  dungeon = [[c.red + '#' for x in range(25)] for y in range(25)]
+
+  seed_x = random.randint(0,9)
+  seed_y = random.randint(0,9)
+
+
+  path_tiles = [[seed_y,seed_x]]
+  branches = [[seed_y,seed_x,None]]
+  dead_ends = []
   
-  exit_x = random.randint(0,9) 
-  exit_y = random.randint(0,9)
-
-  p_x = random.randint(0,9)
-  p_y = random.randint(0,9)
-
-  dungeon[p_y][p_x] = c.blue + '@' + c.base3
-
-  path_tiles = [[p_y,p_x]]
-  branches= [[p_y,p_x]]
-  
-  for branch in branches:
-    print('Branching from branch ' + str(branch))
-    dir = 1
+  while True:
+    branch = random.choice(branches)
+    branches.remove(branch)
+    directions = [1,2,3,4]
+    expanded = False
+    trials = 0
     while True:
-      # 1 = up, 2 = right, 3 = down, 4 = left
+      trials += 1
+      dir = random.choice(directions)
+      directions.remove(dir)
       if dir == 1: # up
-        print('  Branching up...')
-        if branch[0] == 0 or [branch[0]-2,branch[1]] in path_tiles:
-          print('    Branch ' + str(branch) + ' Cannot go up.')
-        elif branch[0] == 1 or [branch[0]-3,branch[1]]:
-          print('    Branch ' + str(branch) + ' Can only go up one space.')
+        if branch[0] == 0 or [branch[0]-2,branch[1]] in path_tiles or branch[2] == 'up':
+          pass
+        elif branch[0] == 1 or [branch[0]-3,branch[1]] in path_tiles:
           path_tiles.append([branch[0]-1,branch[1]])
-          branches.append([branch[0]-1,branch[1]])
+          dead_ends.append([branch[0]-1,branch[1]])
+          expanded = True
         else:
-          print('    Branch ' + str(branch) + ' Can go up two spaces.')
           path_tiles.append([branch[0]-1,branch[1]])
           path_tiles.append([branch[0]-2,branch[1]])
-          branches.append([branch[0]-2,branch[1]])
-            
+          branches.append([branch[0]-2,branch[1],'up'])
+          expanded = True
+          
       elif dir == 2: # right
-        print('  Branching right...')
-        if branch[1] == 9 or [branch[0],branch[1]+2] in path_tiles:
-          print('    Branch ' + str(branch) + ' Cannot go right.')
-        elif branch[1] == 8 or [branch[0],branch[1]+3] in path_tiles:
-          print('    Branch ' + str(branch) + ' Can only go right one space.')
+        if branch[1] == 24 or [branch[0],branch[1]+2] in path_tiles or branch[2] == 'right':
+          pass
+        elif branch[1] == 23 or [branch[0],branch[1]+3] in path_tiles:
           path_tiles.append([branch[0],branch[1]+1])
-          branches.append([branch[0],branch[1]+1])
+          dead_ends.append([branch[0],branch[1]+1])
+          expanded = True
         else:
-          print('    Branch ' + str(branch) + ' Can go right two spaces.')
           path_tiles.append([branch[0],branch[1]+1])
           path_tiles.append([branch[0],branch[1]+2])
-          branches.append([branch[0],branch[1]+2])
+          branches.append([branch[0],branch[1]+2,'right'])
+          expanded = True
             
       elif dir == 3: # down
-        print('  Branching down...')
-        if branch[0] == 9 or [branch[0]+2,branch[1]] in path_tiles:
-          print('    Branch ' + str(branch) + ' Cannot go down.')
-        elif branch[0] == 8 or [branch[0]+3,branch[1]] in path_tiles:
-          print('    Branch ' + str(branch) + ' Can only go down one space.')
+        if branch[0] == 24 or [branch[0]+2,branch[1]] in path_tiles or branch[2] == 'down':
+          pass
+        elif branch[0] == 23 or [branch[0]+3,branch[1]] in path_tiles:
           path_tiles.append([branch[0]+1,branch[1]])
-          branches.append([branch[0]+1,branch[1]])
+          dead_ends.append([branch[0]+1,branch[1]])
+          expanded = True
         else:
-          print('    Branch ' + str(branch) + ' Can go down two spaces.')
           path_tiles.append([branch[0]+1,branch[1]])
           path_tiles.append([branch[0]+2,branch[1]])
-          branches.append([branch[0]+2,branch[1]])
+          branches.append([branch[0]+2,branch[1],'down'])
+          expanded = True
           
       elif dir == 4: # left
-        print('  Branching left...')
-        if branch[1] == 0 or [branch[0],branch[1]-2] in path_tiles:
-          print('    Branch ' + str(branch) + ' Cannot go left.')
+        if branch[1] == 0 or [branch[0],branch[1]-2] in path_tiles or branch[2] == 'left':
+          pass
         elif branch[1] == 1 or [branch[0],branch[1]-3] in path_tiles:
-          print('    Branch ' + str(branch) + ' Can only go left one space.')
           path_tiles.append([branch[0],branch[1]-1])
-          branches.append([branch[0],branch[1]-1])
+          dead_ends.append([branch[0],branch[1]-1])
+          expanded = True
         else:
-          print('    Branch ' + str(branch) + ' Can go left two spaces.')
           path_tiles.append([branch[0],branch[1]-1])
           path_tiles.append([branch[0],branch[1]-2])
-          branches.append([branch[0],branch[1]-2])
-      time.sleep(1)
-      dir += 1
-      if dir == 5:
+          branches.append([branch[0],branch[1]-2,'left'])
+          expanded = True
+      elif trials == 3 and expanded:
         break
+      elif trials == 4 and expanded:
+        break
+      if directions == []:
+        break
+    if branches == []:
+      break
 
   for tile in path_tiles:
     dungeon[tile[0]][tile[1]] = c.base3 + ' '
   
-  dungeon[path_tiles[-1][0]][path_tiles[-1][1]] = c.green + '$'
+  exit = random.choice(path_tiles)
+  dungeon[exit[0]][exit[1]] = c.green + '$'
+  while True:
+    start = random.choice(path_tiles)
+    if start != exit:
+      break
+  dungeon[start[0]][start[1]] = c.blue + '@'
   
 def moving():
-  last_tile_rep = c.base3 + ' '
+  last_tile_rep = c.base03 + '$'
   while True:
     clear()
-    for y_find in range(10):
-      for x_find in range(10):
+    for y_find in range(25):
+      for x_find in range(25):
         if '@' in dungeon[y_find][x_find]:
           p_y = y_find
           p_x = x_find
           
-    print(c.base3 + ' __________ ')
+    print(c.base3 + ' _________________________ ')
     for pr_y in dungeon:
       print(c.base3 + '|',end='')
       for pr_x in pr_y:
         print(pr_x,end='')
       print(c.base3 + '|')
-    print(c.base3 + ' ---------- ')
+    print(c.base3 + ' ------------------------- ')
 
     try:
       mv = input('>>> ')
@@ -156,7 +164,7 @@ def moving():
           dungeon[p_y][p_x] = c.blue + '@'
       except IndexError:
         pass
-    if '$' in last_tile_rep:
+    if c.green + '$' in last_tile_rep:
       clear()
       input('Fin.')
       break
