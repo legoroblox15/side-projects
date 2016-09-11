63dd6ca4ca9f213cd897b2a9b0612dfd3d69a620
Chris Mason <littlecmason@yahoo.com>
2016-09-11 19:10:35 -0400

    save

--- dungeon.py
+++ dungeon.py
@@ -6,12 +6,12 @@ import time
 def clear():
   print(c.clear,end='')
 
-def build_dungeon():
-  dungeon = [[c.red + '#' for x in range(25)] for y in range(25)]
-  path_tiles = [[12,12]]
-  branches = [[12,12,None]]
-  dead_ends = []
-  colors = [c.yellow, c.orange, c.red, c.magenta, c.violet, c.blue, c.cyan, c.green]
+def set_path():  
+  seed_x = random.randint(0,24)
+  seed_y = random.randint(0,24)
+  
+  path_tiles = [[seed_x,seed_y]]
+  branches = [[seed_x,seed_y,None]]
   
   while True:
     branch = random.choice(branches)
@@ -28,7 +28,6 @@ def build_dungeon():
           pass
         elif branch[0] == 1 or [branch[0]-3,branch[1]] in path_tiles:
           path_tiles.append([branch[0]-1,branch[1]])
-          dead_ends.append([branch[0]-1,branch[1]])
           expanded = True
         else:
           path_tiles.append([branch[0]-1,branch[1]])
@@ -41,7 +40,6 @@ def build_dungeon():
           pass
         elif branch[1] == 23 or [branch[0],branch[1]+3] in path_tiles:
           path_tiles.append([branch[0],branch[1]+1])
-          dead_ends.append([branch[0],branch[1]+1])
           expanded = True
         else:
           path_tiles.append([branch[0],branch[1]+1])
@@ -54,7 +52,6 @@ def build_dungeon():
           pass
         elif branch[0] == 23 or [branch[0]+3,branch[1]] in path_tiles:
           path_tiles.append([branch[0]+1,branch[1]])
-          dead_ends.append([branch[0]+1,branch[1]])
           expanded = True
         else:
           path_tiles.append([branch[0]+1,branch[1]])
@@ -67,7 +64,6 @@ def build_dungeon():
           pass
         elif branch[1] == 1 or [branch[0],branch[1]-3] in path_tiles:
           path_tiles.append([branch[0],branch[1]-1])
-          dead_ends.append([branch[0],branch[1]-1])
           expanded = True
         else:
           path_tiles.append([branch[0],branch[1]-1])
@@ -78,24 +74,47 @@ def build_dungeon():
         break
     if branches == []:
       break
+  return [path_tiles,branches]
 
+def build_dungeon():
+  darkness = ''
+  dungeon = [[darkness + c.red + '█' for x in range(25)] for y in range(25)]
+  
+  path_tiles = set_path()
+  branches = path_tiles.pop()
+  path_tiles = path_tiles.pop()
+    
   for tile in path_tiles:
     dungeon[tile[0]][tile[1]] = c.base3 + ' '
   
-  exit = random.choice(path_tiles)
-  dungeon[exit[0]][exit[1]] = c.green + '$'
+  dungeon[path_tiles[-1][0]][path_tiles[-1][1]] = darkness + c.green + '$'
   while True:
     start = random.choice(path_tiles)
-    if start != exit:
+    if start != [path_tiles[-1][0],path_tiles[-1][1]]:
       break
-  dungeon[start[0]][start[1]] = c.blue + '@'
+  dungeon[start[0]][start[1]] = c.blue + '●'
+  
+  path_count = -1
+  for tile in path_tiles:
+    path_count += 1
+  
+  colors = [c.yellow, c.magenta, c.violet, c.blue, c.cyan, c.green, c.base00, c.base01] 
+  for color in colors:
+    while True:
+      key = random.randint(0,path_count)
+      door = random.randint(key+1,path_count)
+      if ' ' in dungeon[path_tiles[key][0]][path_tiles[key][1]] and ' ' in dungeon[path_tiles[door][0]][path_tiles[door][1]]:
+        dungeon[path_tiles[key][0]][path_tiles[key][1]] = darkness + color + '%'
+        dungeon[path_tiles[door][0]][path_tiles[door][1]] = darkness + colors + '&'
+
   return dungeon
   
 def moving(dungeon):
+  tattle = "It sure is dark"
   last_tile_rep = c.base03 + '$'
   for y_find in range(25):
     for x_find in range(25):
-      if '@' in dungeon[y_find][x_find]:
+      if '●' in dungeon[y_find][x_find]:
         p_y = y_find
         p_x = x_find
           
@@ -134,6 +153,8 @@ def moving(dungeon):
         else:
           print(pr_x,end='')
       print(c.base3 + '│')
+    print(c.base3 + '├─────────────────────────┤')
+    print(c.base3 + '│'+c.base01+' *** '+tattle+' *** '+c.base3+'│') # Tattles need to be a total of 15 standard font widthed characters long
     print(c.base3 + '└─────────────────────────┘')
 
     try:
@@ -143,42 +164,42 @@ def moving(dungeon):
       continue
     if mv == 'w':
       try:
-        if '#' not in dungeon[p_y-1][p_x] and p_y-1 >= 0:
+        if '█' not in dungeon[p_y-1][p_x] and p_y-1 >= 0:
           last_tile = dungeon[p_y-1][p_x]
           dungeon[p_y][p_x] = last_tile_rep
           last_tile_rep = last_tile
           p_y -= 1
-          dungeon[p_y][p_x] = c.blue + '@'
+          dungeon[p_y][p_x] = c.blue + '●'
       except IndexError:
         pass
     elif mv == 'a':
       try:
-        if '#' not in dungeon[p_y][p_x-1] and p_x-1 >= 0:
+        if '█' not in dungeon[p_y][p_x-1] and p_x-1 >= 0:
           last_tile = dungeon[p_y][p_x-1]
           dungeon[p_y][p_x] = last_tile_rep
           last_tile_rep = last_tile
           p_x -= 1
-          dungeon[p_y][p_x] = c.blue + '@'
+          dungeon[p_y][p_x] = c.blue + '●'
       except IndexError:
         pass
     elif mv == 's':
       try:
-        if '#' not in dungeon[p_y+1][p_x]:
+        if '█' not in dungeon[p_y+1][p_x]:
           last_tile = dungeon[p_y+1][p_x]
           dungeon[p_y][p_x] = last_tile_rep
           last_tile_rep = last_tile
           p_y += 1
-          dungeon[p_y][p_x] = c.blue + '@'
+          dungeon[p_y][p_x] = c.blue + '●'
       except IndexError:
         pass
     elif mv == 'd':
       try:
-        if '#' not in dungeon[p_y][p_x+1]:
+        if '█' not in dungeon[p_y][p_x+1]:
           last_tile = dungeon[p_y][p_x+1]
           dungeon[p_y][p_x] = last_tile_rep
           last_tile_rep = last_tile
           p_x += 1
-          dungeon[p_y][p_x] = c.blue + '@'
+          dungeon[p_y][p_x] = c.blue + '●'
       except IndexError:
         pass
     if c.green + '$' in last_tile_rep:
