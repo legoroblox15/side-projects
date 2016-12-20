19bf06e3091295f37439e757647d9408da6320c2
Chris Mason <littlecmason@yahoo.com>
2016-12-20 13:40:11 -0500

    save

--- connect-four.py
+++ connect-four.py
@@ -6,7 +6,7 @@ from getch import getch
 def clear():
   print(c.clear,end='')
 
-def game_has_not_been_beat_yet_do_not_worry_you_can_still_continue_giving_turns(board):
+def gamehasnotbeenbeatyetdonotworryyoucanstillcontinuegivingturns(board):
   for y in range(6):    #   Checks horizontal wins
     for x in range(3):
       if board[y][x] == board[y][x+1] == board[y][x+2] == board[y][x+3] == c.red + '█':
@@ -14,33 +14,39 @@ def game_has_not_been_beat_yet_do_not_worry_you_can_still_continue_giving_turns(
       elif board[y][x] == board[y][x+1] == board[y][x+2] == board[y][x+3] == c.blue + '█':
         return 'blue'
 
-  for y in range(2):    #   Checks vertical wins
-    for x in range(7):
+  for y in range(3):    #   Checks vertical wins
+    for x in range(6):
       if board[y][x] == board[y+1][x] == board[y+2][x] == board[y+3][x] == c.red + '█': 
         return 'red'
       elif board[y][x] == board[y+1][x] == board[y+2][x] == board[y+3][x] == c.blue + '█': 
         return 'blue'
 
-  for y in range(2):    #   Checks upper left to lower right diagonal wins
-    for x in range(3):
+  for y in range(3):    #   Checks upper left to lower right diagonal wins
+    for x in range(4):
       if board[y][x] == board[y+1][x+1] == board[y+2][x+2] == board[y+3][x+3] == c.red + '█': 
         return 'red'
       elif board[y][x] == board[y+1][x+1] == board[y+2][x+2] == board[y+3][x+3] == c.blue + '█':
         return 'blue'
 
+  for y in range(3):    #   Checks upper right to lower left diagonal wins
+    for x in range(4):
+      if board[y][x+3] == board[y+1][x+2] == board[y+2][x+1] == board[y+3][x] == c.red + '█': 
+        return 'red'
+      elif board[y][x+3] == board[y+1][x+2] == board[y+2][x+1] == board[y+3][x] == c.blue + '█':
+        return 'blue'
 board = [[' ' for x in range(7)] for y in range(6)]
 turn = 0
 
 while True:
   clear()
-  verdict = game_has_not_been_beat_yet_do_not_worry_you_can_still_continue_giving_turns(board)
+  verdict = gamehasnotbeenbeatyetdonotworryyoucanstillcontinuegivingturns(board)
+  
   if verdict == 'red':
-    clear()
-    print(c.red + '█' + c.reset + ' Wins!'
+    print('Red wins!')
     break
   elif verdict == 'blue':
-    clear()
-    print(c.blue + '█' + c.reset + ' Wins!'
+    print('Blue wins!')
+    break
 
   for y in board:
     print(c.base3 + '│',end='')
@@ -55,6 +61,7 @@ while True:
     move = getch()
     if move == '\x1b':
       verdict = 'Hit escape (or the arrow keys idk because pygetch is a silly goose)'
+      clear()
       break
     
     elif move not in '1234567':
@@ -68,10 +75,11 @@ while True:
     token = c.red + '█'
 
   else:
-    print('\n'+c.blue+'█'+c.base3+"'s turn\n>>> ",end='')
+    print('\n'+c.blue+'█'+c.base3+"'s turn",end='')
     move = getch()
     if move == '\x1b':
       verdict = 'Hit escape (or the arrow keys idk because pygetch is a silly goose)'
+      clear()
       break
  
     elif move not in '1234567':
@@ -89,4 +97,3 @@ while True:
     if board[top][move] == ' ':
       board[top][move] = token
       break
-clear()
